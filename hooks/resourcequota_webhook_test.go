9803bac520dd15111501e73a4032c38e1dcfe507
takonomura <takonomura@users.noreply.github.com>
2022-08-26 06:00:52 +0000

    Add webhook test
    Signed-off-by: takonomura <takonomura@users.noreply.github.com>

---
+++ hooks/resourcequota_webhook_test.go
@@ -0,0 +1,87 @@
+package hooks
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	necotiatorv1beta1 "github.com/cybozu-go/necotiator/api/v1beta1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	m           sync.Mutex
+	testCounter int32
+)
+
+func newTestObjectName() string {
+	m.Lock()
+	defer m.Unlock()
+	testCounter += 1
+	return fmt.Sprintf("test-%d", testCounter)
+}
+
+var _ = Describe("Webhook Test", func() {
+	ctx := context.Background()
+
+	It("should deny exceeded quota", func() {
+		namespaceName := newTestObjectName()
+		namespace := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: namespaceName,
+			},
+		}
+		err := k8sClient.Create(ctx, namespace)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		tenantResourceQuotaName := newTestObjectName()
+		tenantResourceQuota := &necotiatorv1beta1.TenantResourceQuota{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: tenantResourceQuotaName,
+			},
+			Spec: necotiatorv1beta1.TenantResourceQuotaSpec{
+				Hard: corev1.ResourceList{
+					"limits.cpu": resource.MustParse("500m"),
+				},
+			},
+		}
+		err = k8sClient.Create(ctx, tenantResourceQuota)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		tenantResourceQuota.Status = necotiatorv1beta1.TenantResourceQuotaStatus{
+			Allocated: map[corev1.ResourceName]necotiatorv1beta1.ResourceUsage{
+				"limits.cpu": {
+					Total: resource.MustParse("0"),
+					Namespaces: map[string]resource.Quantity{
+						namespaceName: resource.MustParse("0"),
+					},
+				},
+			},
+		}
+		err = k8sClient.Status().Update(ctx, tenantResourceQuota)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		resourceQuota := &corev1.ResourceQuota{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "default",
+				Namespace: namespaceName,
+				Labels: map[string]string{
+					"app.kubernetes.io/created-by": "necotiator",
+					"necotiator.cybozu.io/tenant":  tenantResourceQuotaName,
+				},
+			},
+			Spec: corev1.ResourceQuotaSpec{
+				Hard: corev1.ResourceList{
+					"limits.cpu": resource.MustParse("600m"),
+				},
+			},
+		}
+		err = k8sClient.Create(ctx, resourceQuota)
+		Expect(err).Should(HaveStatusErrorReason(Equal(metav1.StatusReasonInvalid)))
+		Expect(err).Should(HaveStatusErrorMessage(ContainSubstring(fmt.Sprintf("exceeded tenant quota: %s, requested: limits.cpu=600m, limited: limits.cpu=500m", tenantResourceQuotaName))))
+	})
+})
